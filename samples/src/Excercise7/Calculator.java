f954fe05b97d97341c6caa848998971291dfb44f
hannan yusop <nan_s96@yahoo.com>
2021-03-18 22:42:14 +0800

    uncomplete ex7

---
+++ samples/src/Excercise7/Calculator.java
@@ -0,0 +1,24 @@
+package Excercise7;
+
+public class Calculator {
+	
+	private int weight = 10;
+
+	public static void main (String[] args) {
+		System.out.println("Weight :");
+				
+	}
+
+
+	public void mars(int weight) {
+		
+		
+	}
+	
+	public void earth(int weight) {
+		
+		
+	}
+}
+
+
